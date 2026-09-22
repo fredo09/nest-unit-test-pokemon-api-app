35976c795c7b9caf12a84362448df00c83356526
fredo09 <fred_15@outlook.com>
2026-09-22 11:52:12 -0600

    feat: unit test initial for pokmeons controllers

--- src/pokmeons/pokmeons.controller.spec.ts
+++ src/pokmeons/pokmeons.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokmeonsController } from './pokmeons.controller';
 import { PokmeonsService } from './pokmeons.service';
+import { PaginationDto } from 'src/shared';
 
 describe('PokmeonsController', () => {
   let controller: PokmeonsController;
+  let service: PokmeonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,20 @@ describe('PokmeonsController', () => {
     }).compile();
 
     controller = module.get<PokmeonsController>(PokmeonsController);
+    service = module.get<PokmeonsService>(PokmeonsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should execute findAll', async () => {
+    const pokmeonPagination: PaginationDto = { limit: 10, page: 1 };
+
+    //* Espia para ver si se llamo el findAll del servicio que ejecuta el findAll del controlador
+    const findAllSpy = jest.spyOn(service, 'findAll');
+
+    await controller.findAll(pokmeonPagination);
+    expect(findAllSpy).toHaveBeenCalledWith(pokmeonPagination);
   });
 });
